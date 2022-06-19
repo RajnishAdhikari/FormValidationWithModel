ef00cc32180ecdba1e3f2a7dc46237e6ad2bc648
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-19 21:34:48 +0545

    Add project files.

---
+++ FormValidationWithModel/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormValidationWithModel.Models;
+namespace FormValidationWithModel.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SubmitData(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ FormValidationWithModel/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace FormValidationWithModel.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please Enter Your Roll")]
+        public int Roll { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Please ReEnter Your Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and Repassword doesn't match")]
+        public string RePassword { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Age")]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Phone")]
+        public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Address")]
+        public string Address { get; set; }
+    }
+}
